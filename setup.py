9a4b3a61f1f7696eba934183fe2988fb38b3f2a2
Vincent Ollivier <contact@vincentollivier.com>
2010-11-27 18:18:07 +0100

    Fix setup

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 
 from distutils.core import setup
 
-setup(name='Memorious',
+setup(name='DaSort',
     version='0.0.1',
-    description='A simple CLI to manage website accounts',
+    description='A CLI tool for sorting and grouping picture by date',
     author='Vincent Ollivier',
     author_email='contact@vincentollivier.com',
-    url='https://github.com/vinc/memorious',
-    packages=['memorious'],
-    scripts=['scripts/memorious']
+    url='https://github.com/vinc/dasort',
+    packages=['dasort'],
+    scripts=['scripts/dasort']
 )
